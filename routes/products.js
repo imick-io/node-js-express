7c60bcc0e2868b28a94ec44cf00e923f43516de2
mboutin-toptal <michael@imick.io>
2024-03-09 09:12:16 -0500

    Added a post request

--- routes/products.js
+++ routes/products.js
@@ -8,4 +8,9 @@ router.get("/", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "products.html"));
 });
 
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 module.exports = router;
