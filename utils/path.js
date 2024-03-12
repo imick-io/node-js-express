e2942b8e06e1d5ded005719d8755353364bfa8d6
mboutin-toptal <michael@imick.io>
2024-03-12 09:44:21 -0400

    Added comment for path file

--- utils/path.js
+++ utils/path.js
@@ -1,3 +1,7 @@
+// On Linux, the path uses / instead of \ as the separator
+// On Windows, the path uses \ instead of / as the separator
+// With path.join, we can use the correct separator for the OS
+
 const path = require("path");
 
 module.exports = path.dirname(require.main.filename);
